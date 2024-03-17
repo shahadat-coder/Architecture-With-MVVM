2b2521c2358a44f1c1a84fd962254e66901b162a
Md.Shahadat Hossain <112892447+shahadat349850@users.noreply.github.com>
2024-03-17 12:11:02 +0600

    Start Adding Routes Manager

---
+++ lib/presentation/login/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LonginView extends StatefulWidget {
+  const LonginView({super.key});
+
+  @override
+  State<LonginView> createState() => _LonginViewState();
+}
+
+class _LonginViewState extends State<LonginView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
